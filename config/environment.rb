a25468edc9953711c5ba2aee89eeae92fe054155
Sinisa Soldatovic <ssoldatovic@yahoo.com>
2015-01-29 07:36:33 +0100

    login - sa greskom

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "drzava.latifundija",
+    password: "dacha1999", enable_starttls_auto: true
+}
+end
